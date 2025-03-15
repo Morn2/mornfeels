3bf07bb4975a88c410d94991b3da7031e946342e
Morn2 <aaron13@bluewin.ch>
2025-03-15 18:15:54 +0100

    splittet main.py out in 3 files

--- git_sync.sh
+++ git_sync.sh
@@ -9,5 +9,8 @@ commit_message=${1:-"Non specific commit message"}
 # Führe git commit aus
 git commit -m "$commit_message"
 
+# Aktuellen Branch ermitteln
+current_branch=$(git rev-parse --abbrev-ref HEAD)
+
 # Führe git push aus
 git push origin "$current_branch"
